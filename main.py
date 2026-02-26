1466e17e4e7ea65734739fbfa08617e9ae5142b2
Arundhati Das <arundhutidas2000@gmail.com>
2026-02-25 19:19:55 -0500

    feat: enable user input for content generation in chatbot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 load_dotenv()
@@ -10,10 +11,14 @@ def main():
     print("Hello from mykawaiagent!")
     if api_key is None:
         raise RuntimeError("RuntimeError")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+# Now we can access `args.user_prompt`
     client = genai.Client(api_key=api_key)
     response= client.models.generate_content(
         model='gemini-2.5-flash',
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=args.user_prompt
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
