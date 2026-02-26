cc47096d0474852e99e539205640c55ed8f1b965
Arundhuti2000 <arundhutidas2000@gmail.com>
2026-02-26 17:06:16 -0500

    feat: update content generation to use structured message format and add verbose output option

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 
 load_dotenv()
@@ -13,15 +14,19 @@ def main():
         raise RuntimeError("RuntimeError")
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 # Now we can access `args.user_prompt`
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     client = genai.Client(api_key=api_key)
     response= client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=args.user_prompt
+        contents=messages
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
