f616becde7e1f1d64ee3e93285229f6d5fecbf85
Arundhuti2000 <arundhutidas2000@gmail.com>
2026-02-27 11:17:53 -0500

    feat: enhance get_files_info function to return detailed file information and add test script

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,4 +5,19 @@ def get_files_info(working_directory, directory="."):
     target_dir=os.path.normpath(os.path.join(working_dir_abs, directory))
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
     if valid_target_dir == False:
-        raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if os.path.isdir(target_dir) == False:
+        return f'Error: "{directory}"'
+    try:
+        listoftargetdiritems=os.listdir(target_dir)
+        listofitemdetails = []
+        for item in listoftargetdiritems:
+            item_path = os.path.join(target_dir, item)
+            is_dir= os.path.isdir(item_path)
+            file_size= os.path.getsize(item_path)
+            full_line = f'- {item}: file_size={file_size} bytes, is_dir={is_dir}\n'
+            listofitemdetails.append(full_line)
+        return "".join(listofitemdetails)
+    except Exception as e:
+        return (f'"Error: {e}"')
