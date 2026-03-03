6eaac74d9f9693ee2809b50cab65abf8d3c524a5
Arundhati Das <arundhutidas2000@gmail.com>
2026-03-02 19:42:09 -0500

    feat: enhance run_python_file function with subprocess execution and error handling; add tests for various scenarios

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,13 +1,32 @@
 import os
+import subprocess
 
 def run_python_file(working_directory, file_path, args=None):
     try:
         working_dir_abs = os.path.abspath(working_directory)
         target_path = os.path.abspath(os.path.join(working_dir_abs, file_path))
         if os.path.commonpath([working_dir_abs, target_path]) != working_dir_abs:
-            return f'Error: Cannot execute  "{file_path}" as it is outside the permitted working directory'
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
         if not os.path.isfile(target_path):
-             return f'Error: File not found or is not a regular file: "{file_path}"'
+             return f'Error: "{file_path}" does not exist or is not a regular file'
         if not target_path.endswith('py'):
             return f'Error: "{file_path}" is not a Python file'
-        command = ["python", ]
+        command = ["python", target_path]
+        if args:
+            command.extend(args)
+        CWD= working_directory
+        CompletedProcess = subprocess.run(command,cwd=CWD, text=True,capture_output=True,timeout=30)
+        
+        output_parts=[]
+        if CompletedProcess.returncode!=0:
+            output_parts.append(f"Process exited with code X")
+        if not CompletedProcess.stderr and not CompletedProcess.stdout:
+            output_parts.append("No output produced")
+        else:
+            if CompletedProcess.stdout:
+                output_parts.append(f"STDOUT: {CompletedProcess.stdout}")
+            if CompletedProcess.stderr:
+                output_parts.append(f"STDERR:  + {CompletedProcess.stderr}")
+        return "\n".join(output_parts)
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 import argparse
 from google.genai import types
-
+from prompts import system_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -21,7 +21,8 @@ def main():
     client = genai.Client(api_key=api_key)
     response= client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
     )
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
